d4103281ea84f9316d3cfe79d2d8f85fc6814a11
George K <koniarig@wlhs.wlwv.k12.or.us>
2025-03-31 16:14:38 +0000

    Simon Claw

---
+++ Dancing_Cat.py
@@ -0,0 +1,23 @@
+import time, sys
+
+def typewriter(text):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+typewriter("My name is Simon Claw... \n")
+
+
+from ascii_magic import AsciiArt
+from PIL import Image
+
+Claw = Image.open("/workspaces/Computer-Programming-2/simonclaw.jpeg")
+
+# Convert to ASCII and print to terminal
+ascii_art = AsciiArt.from_pillow_image(Claw)
+ascii_art.to_terminal()
+
+
+
+
